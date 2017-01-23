072a348d5345fd3680a1ca280cacddca8942889e
Nicholas Deagon <ndeagon@ucsd.edu>
2017-01-22 21:02:08 -0800

    Added gaze selection for laser

--- Assets/Scripts/GazeSystem.cs
+++ Assets/Scripts/GazeSystem.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class GazeSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public const int NONE = 0;
+    public const int LASER = 1;
+    public const int CANNON = 2;
+    
+    // Use this for initialization
+    void Start () {
 		
 	}
 
@@ -13,18 +17,20 @@ public class GazeSystem : MonoBehaviour {
     float timer = 2.0f;
     static GameObject curBrick;
     static GameObject prevBrick;
+    static int mode = NONE;
 	
 	// Update is called once per frame
 	void Update () {
         Ray gaze = new Ray(transform.position, transform.forward);
         // cant figure out how to get a lazer to show up...
-       // Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.green, 5);
+        // Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.green, 5);
         RaycastHit rayHit;
 
         if(Physics.Raycast(gaze, out rayHit, Mathf.Infinity))
         {
            // Debug.LogFormat("You hit {0}!", rayHit.collider.name);
-            if(rayHit.transform.gameObject.tag == "brick")
+
+            if(rayHit.transform.gameObject.tag == "brick" && mode == LASER)
             {
                 prevBrick = curBrick;
                 curBrick = rayHit.transform.gameObject;
@@ -33,17 +39,31 @@ public class GazeSystem : MonoBehaviour {
                     timer -= Time.deltaTime;
                 } else
                 {
-                    Debug.Log("Resetting Timer");
                     timer = 2.0f;
                 }
 
-                if(timer < 0f)
+                if (timer < 0f)
                 {
                     Destroy(rayHit.transform.gameObject);
                 }
-                //Debug.LogFormat("You hit a brick!");
+
             }
+            else if(rayHit.transform.gameObject.tag == "laser")
+            {
+                timer -= Time.deltaTime;
+
+                if (timer < 0f)
+                {
+                    Debug.Log("Laser Selected");
+
+                    mode = LASER;
+                }
+            } else
+            {
+                timer = 2.0f;
+            }
+
         }
-		
-	}
+
+    }
 }
