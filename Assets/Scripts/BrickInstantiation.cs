2212043885329e8038b8b4f283c021d8b6152707
Nicholas Deagon <ndeagon@ucsd.edu>
2017-01-22 16:58:09 -0800

    Created Brick Cylinder around Camera

---
+++ Assets/Scripts/BrickInstantiation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickInstantiation : MonoBehaviour {
+
+    public Transform brickPrefab;
+    MeshRenderer thisRenderer;
+
+    int numberOfObjects = 32;
+    float radius = 4f;
+    int cylinderHeight = 17;
+
+    float brickOffset = 0.75f; // pretty random value... trying to offset the next layer by half a brick
+                               // but this is basically spinning the circle by this many radians
+
+    void Start()
+    {
+        for (int yi = 0; yi < cylinderHeight; yi++)
+        {
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                float angle = (i * Mathf.PI * 2 / numberOfObjects) + brickOffset;
+                float x = Mathf.Cos(angle) * radius;
+                float z = Mathf.Sin(angle) * radius;
+                float y = 0.15f + (0.3f * yi);
+
+                Vector3 pos = new Vector3(x, y, z);
+                Transform brick = Instantiate(brickPrefab, pos, Quaternion.identity);
+
+                brick.Rotate(new Vector3(0, -1 * Mathf.Rad2Deg * angle, 0));
+                brick.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", GetBrickColor());
+
+            }
+
+            brickOffset = -1 * brickOffset;
+        }
+    }
+
+    Color GetBrickColor()
+    {
+        switch(Random.Range(1, 4))
+        {
+            case 1:
+                return new Color(0.5955882f, 0.1886035f, 0.09196584f);
+            case 2:
+                return new Color(0.6176471f, 0.3093938f, 0.1725779f);
+            case 3:
+                return new Color(0.625f, 0.4684497f, 0.3860294f);
+            default:
+                return new Color(0.5955882f, 0.1886035f, 0.09196584f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
