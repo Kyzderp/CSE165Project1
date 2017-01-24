1c59a7a0e559e34277222a449ad9810af5ef3990
Kyzderp <hannah_chu@yahoo.com>
2017-01-24 03:36:11 -0800

    I think I added too much stuff?

--- Assets/Scripts/GazeSystem.cs
+++ Assets/Scripts/GazeSystem.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class GazeSystem : MonoBehaviour {
 
+    public Transform cannonballPrefab;
+
     public const int NONE = 0;
     public const int LASER = 1;
     public const int CANNON = 2;
+
+    const float dwellTime = 1.5f;
+    const float cannonInterval = 3.0f;
     
     // Use this for initialization
     void Start () {
 		
 	}
 
-    int i = 0;
-    float timer = 2.0f;
+    float timer = dwellTime;
+    float cannonTimer = cannonInterval;
     static GameObject curBrick;
     static GameObject prevBrick;
     static int mode = NONE;
@@ -23,10 +28,11 @@ public class GazeSystem : MonoBehaviour {
 	void Update () {
         Ray gaze = new Ray(transform.position, transform.forward);
         // cant figure out how to get a lazer to show up...
-        // Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.green, 5);
+        //Vector3 v = new Vector3(transform.forward.x, transform.forward.y, transform.forward.z);
+        Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.green);
         RaycastHit rayHit;
 
-        if(Physics.Raycast(gaze, out rayHit, Mathf.Infinity))
+        if (Physics.Raycast(gaze, out rayHit, Mathf.Infinity))
         {
            // Debug.LogFormat("You hit {0}!", rayHit.collider.name);
 
@@ -34,19 +40,17 @@ public class GazeSystem : MonoBehaviour {
             {
                 prevBrick = curBrick;
                 curBrick = rayHit.transform.gameObject;
+
                 if(curBrick == prevBrick)
-                {
                     timer -= Time.deltaTime;
-                } else
-                {
-                    timer = 2.0f;
-                }
+                else
+                    timer = dwellTime;
 
                 if (timer < 0f)
                 {
+                    Debug.Log("destroy brick");
                     Destroy(rayHit.transform.gameObject);
                 }
-
             }
             else if(rayHit.transform.gameObject.tag == "laser")
             {
@@ -58,12 +62,39 @@ public class GazeSystem : MonoBehaviour {
 
                     mode = LASER;
                 }
-            } else
-            {
-                timer = 2.0f;
             }
+            else if (rayHit.transform.gameObject.tag == "cannon")
+            {
+                timer -= Time.deltaTime;
 
+                if (timer < 0f)
+                {
+                    Debug.Log("Cannon Selected");
+
+                    mode = CANNON;
+                }
+            }
+            else
+            {
+                timer = dwellTime;
+            }
         }
 
+        // Fire cannon periodically if in cannon mode
+        if (mode == CANNON)
+        {
+            cannonTimer -= Time.deltaTime;
+            if (cannonTimer < 0f)
+            {
+                this.fireCannonball();
+                cannonTimer = cannonInterval;
+            }
+        }
+    }
+
+    void fireCannonball()
+    {
+        Transform ball = Instantiate(cannonballPrefab, transform.position, Quaternion.identity);
+        ball.GetComponent<Rigidbody>().velocity = transform.forward * 10.0f;
     }
 }
