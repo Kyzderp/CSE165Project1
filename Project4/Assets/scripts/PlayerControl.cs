facec1ffec6d7ecc5645e75bf10527132696335b
ndeagon <yic105@UCSD.EDU>
2017-03-19 21:03:06 -0700

    added movement and flipped minimap. Added more assets

---
+++ Project4/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public Transform character;
+
+    float MOVE_SPEED = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleMovement();
+	}
+
+    void handleMovement()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.0f)
+        {
+            Vector3 dir = new Vector3(transform.forward.x, 0, transform.forward.z);
+            character.position += dir * Time.deltaTime * MOVE_SPEED;
+        }
+    }
+}
