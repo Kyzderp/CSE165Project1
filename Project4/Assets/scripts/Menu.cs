e82cdc7f41a671b6744bdea28f2e4864d67d6d6c
ndeagon <ndeagon@UCSD.EDU>
2017-03-19 20:48:12 -0700

    some more fixes, static screen

---
+++ Project4/Assets/scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public OVRInput.Controller left;
+    public OVRInput.Controller right;
+
+    enum Screen { Initial, Start, Options };
+    private Screen screen = Screen.Initial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (screen == Screen.Initial)
+        {
+            // TODO: if clicked
+            screen = Screen.Start;
+        }
+
+        if (screen == Screen.Start)
+        {
+            // TODO: if clicked
+            screen = Screen.Options;
+        }
+
+        if (screen == Screen.Options)
+        {
+
+        }
+	}
+}
---
+++ Project4/Assets/scripts/StaticScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticScreen : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float rndXoffset = Random.value;
+        float rndYoffset = Random.value;
+
+        this.gameObject.GetComponent<MeshRenderer>().material.mainTextureOffset = new Vector2(rndXoffset, rndYoffset);
+    }
+}
